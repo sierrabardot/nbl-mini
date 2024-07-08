8546de89148130ea2ef5351f6a0829590eedf4b3
Sierra <sierra.brown@nbl.com.au>
2024-07-08 10:06:45 +1000

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container">
+    <h1>NBL Mini</h1>
+    <p class="subheading">Everything you need to know about the NBL in one place</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel incidunt explicabo assumenda est suscipit esse
+      repudiandae soluta tenetur cumque quod, quidem ipsa perferendis labore facilis, quos quibusdam placeat error.
+      Minus.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'NBL Mini',
+      meta: [
+        { name: 'description', content: 'This is the home page for the NBL Mini website.' },
+        { name: 'keywords', content: 'about NBL, NBL home page, National Basketball League, basketball' },
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/stats/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <h1>Stats</h1>
+    <p class="subheading">Mini Stats</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos quasi magnam dolorum ratione, non accusantium dicta sed asperiores sapiente quae minus rerum voluptatem error perspiciatis iusto eaque optio harum laudantium voluptatum est eligendi ea labore expedita. Reprehenderit ipsa corrupti autem! Corrupti, sapiente facilis? Accusamus dolor provident corrupti ex aliquid consectetur soluta. Fugiat illum accusamus accusantium consectetur iure maxime impedit quas perferendis. Nihil ex, quod consectetur libero, fugit quam, eum explicabo eos architecto iste odio maiores. Ut, sint rerum, mollitia itaque incidunt nesciunt excepturi ab perferendis ad aut nihil rem maiores enim ea id impedit et, qui quae sunt! Ad, enim!</p>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Stats',
+      meta: [
+        { name: 'description', content: 'This is my about description here.'},
+        { name: 'keywords', content: 'NBL, NBL info, national basketball league'},
+      ]
+    }
+  }
+}
+</script>
